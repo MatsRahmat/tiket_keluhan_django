3bb0139e416f880d009195e8e5876d9be664bb66
MatsRahmat <matsrahmathidayat@gmail.com>
2026-04-03 21:36:07 +0700

    Initial project

---
+++ run-dev.bat
@@ -0,0 +1,27 @@
+@echo on
+
+REM Activate venv
+call .venv-3.13\Scripts\activate.bat
+
+REM check pip list
+call python -m pip list
+
+REM check requirements file
+echo checking requirements file
+IF EXIST "requirements.txt" (
+    echo requirements file found, installing new dependecies
+    call python -m pip install -r requirements.txt
+
+) ELSE echo requirements file not found, skip installing from file;
+
+REM Default IP
+set IP=127.0.0.1
+
+REM Setted PORT, change if needed
+set PORT=8800
+echo ======================================
+
+if not "%~1"=="" set "IP=%~1"
+echo start server on %IP%:%PORT%
+
+watchmedo auto-restart --directory=./ --pattern="*.py;*.html" --recursive -- uvicorn manajement_keluhan.asgi:application --host %IP% --port %PORT%
---
+++ tiket_keluhan/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TiketKeluhanConfig(AppConfig):
+    name = 'tiket_keluhan'
